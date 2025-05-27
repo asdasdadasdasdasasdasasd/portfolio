9a2bfea9070aae6bcfa8b3945fede4d4ab727a04
asdasdadasdasdasasdasasd <gr302573@gradia.fi>
2025-05-27 12:14:12 +0300

    feat(svelte-kauppa): julkaisu

---
+++ svelte-kauppa/_app/immutable/nodes/0.5BiXEkdh.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/DZsco5mA.js";import{f as p}from"../chunks/6s6nxGQQ.js";import{s}from"../chunks/DocrPlth.js";const i=!0,l=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,l as universal};
---
+++ svelte-kauppa/harjoitukset/README.md
@@ -0,0 +1,6 @@
+Lisää svelten storybook-harjoituksissa tarvitsemasi kuvat tähän kansioon.
+
+Voit lukea lisää kuvien käytöstä tiedostosta:
+[`/harjoitukset/02-javascript/01-svelte/esimerkit/kuva.svelte`](/harjoitukset/02-javascript/01-svelte/esimerkit/kuva.svelte).
+
+Löydät tiedostosta myös esimerkkikoodin, jolla pystyt käyttämään kuvia.
